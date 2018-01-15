3739243c7a98d4a9cc3586cb2c28fffec0be7678
Aleksi Wikman <aleksi.wikman@gmail.com>
2018-01-15 14:10:14 +0200

    add pipy setup files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='iZettle',
+    packages=['iZettle'],
+    version='0.1',
+    description='',
+    author='Aleksi Wikman',
+    author_email='aleksi@vilkas.fi',
+    url='https://github.com/vilkasgroup/iZettle',
+    #  download_url='https://github.com/vilkasgroup/iZettle',
+    license="MIT license",
+    keywords=['izettle', 'POS', 'Payment'],
+    classifiers=[],
+)
